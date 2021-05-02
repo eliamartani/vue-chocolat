aa256e6416c9d39a8516bf998d6d65e257338b5d
Eliamar Tani <eliamartani@gmail.com>
2021-05-02 13:09:52 +0100

    chore: Add publish script

---
+++ scripts/publish.js
@@ -0,0 +1,80 @@
+const { execSync } = require('child_process')
+const { writeFileSync } = require('fs')
+const { resolve } = require('path')
+const { createInterface } = require('readline')
+const package = require('../package.json')
+
+function exec(command) {
+  execSync(command, { stdio: 'inherit' })
+}
+
+function nodeInstallAndPublish(pkg) {
+  saveFile(pkg)
+  exec('npm install')
+  exec('npm publish')
+}
+
+function publishToGit(pkg, name, publishConfig) {
+  pkg.name = name
+  pkg.publishConfig = publishConfig
+
+  nodeInstallAndPublish(pkg)
+}
+
+function publishToNpm(pkg) {
+  delete pkg.publishConfig
+
+  pkg.name = pkg.name.replace(/(\@(.*?)\/)/i, '')
+
+  nodeInstallAndPublish(pkg)
+}
+
+function saveFile(pkg) {
+  const file = resolve('package.json')
+
+  writeFileSync(file, JSON.stringify(pkg, null, '  '))
+}
+
+function updateVersion(pkg) {
+  let proceed = false
+  const readline = createInterface({
+    input: process.stdin,
+    output: process.stdout
+  })
+
+  readline.question(
+    `Which version to set?\nPrevious version: ${pkg.version}\n> `,
+    version => {
+      if (version) {
+        pkg.version = version
+
+        saveFile(pkg)
+
+        exec('git add package.json')
+        exec(`git commit -m "chore: Bump application version to ${version}"`)
+        exec('git push')
+
+        proceed = true
+      }
+
+      readline.close()
+    }
+  )
+
+  readline.on('close', () => {
+    if (!proceed) {
+      return
+    }
+
+    // make previous copy nothing is lost
+    const { name, publishConfig } = pkg
+
+    publishToNpm(pkg)
+    publishToGit(pkg, name, publishConfig)
+  })
+}
+
+updateVersion(package)
+
+// cleanup
+exec('git checkout .')
