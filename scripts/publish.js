4b6d390864b904bc3e576422bad2119c18bd8742
Eliamar Tani <eliamartani@gmail.com>
2021-05-02 22:44:25 +0100

    chore: Update publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -4,80 +4,130 @@ const { resolve } = require('path')
 const { createInterface } = require('readline')
 const package = require('../package.json')
 
-function exec(command) {
-  execSync(command, { stdio: 'inherit' })
-}
+class PublishScript {
+  constructor(packageJson) {
+    this.package = packageJson
 
-function nodeInstallAndPublish(pkg) {
-  saveFile(pkg)
-  exec('npm install')
-  exec('npm publish')
-}
+    // make previous copy nothing is lost
+    this.name = packageJson.name
+    this.publishConfig = packageJson.publishConfig
 
-function publishToGit(pkg, name, publishConfig) {
-  pkg.name = name
-  pkg.publishConfig = publishConfig
+    this.handleClose = this.handleClose.bind(this)
+    this.handleQuestion = this.handleQuestion.bind(this)
+  }
 
-  nodeInstallAndPublish(pkg)
-}
-
-function publishToNpm(pkg) {
-  delete pkg.publishConfig
-
-  pkg.name = pkg.name.replace(/(\@(.*?)\/)/i, '')
-
-  nodeInstallAndPublish(pkg)
-}
-
-function saveFile(pkg) {
-  const file = resolve('package.json')
-
-  writeFileSync(file, JSON.stringify(pkg, null, '  '))
-}
-
-function updateVersion(pkg) {
-  let proceed = false
-  const readline = createInterface({
-    input: process.stdin,
-    output: process.stdout
-  })
-
-  readline.question(
-    `Which version to set?\nPrevious version: ${pkg.version}\n> `,
-    version => {
-      if (version) {
-        pkg.version = version
-
-        saveFile(pkg)
-
-        exec('npm install')
-        exec('git add package.json')
-        exec('git add package-lock.json')
-        exec(`git commit -m "chore: Bump application version to ${version}"`)
-        exec(`git tag v${version}"`)
-        exec('git push --tags')
-
-        proceed = true
-      }
-
-      readline.close()
+  get packageJsonGit() {
+    return {
+      ...this.package,
+      name: this.name,
+      publishConfig: this.publishConfig
     }
-  )
+  }
 
-  readline.on('close', () => {
-    if (!proceed) {
+  get packageJsonNpm() {
+    return {
+      ...this.package,
+      name: this.package.name.replace(/(\@(.*?)\/)/i, ''),
+      publishConfig: {}
+    }
+  }
+
+  /**
+   * Executes bash script synchronously
+   * @param {string|string[]} command (list of) command script
+   */
+  exec(command) {
+    const commands = Array.isArray(command) ? command : [command]
+    const options = { stdio: 'inherit' }
+
+    commands.forEach(script => execSync(script, options))
+  }
+
+  handleClose() {
+    if (!this.version) {
+      console.log('No version set. Finishing script.')
       return
     }
 
-    // make previous copy nothing is lost
-    const { name, publishConfig } = pkg
+    this.publishToNpm()
+    this.publishToGit()
 
-    publishToNpm(pkg)
-    publishToGit(pkg, name, publishConfig)
-  })
+    // cleanup
+    this.exec('git checkout .')
+  }
+
+  handleQuestion(version) {
+    this.version = version
+
+    if (!this.version) {
+      this.readline.close()
+
+      return
+    }
+
+    this.updatePackageVersion()
+
+    this.readline.close()
+  }
+
+  installAndPublish() {
+    this.exec(['npm install', 'npm publish'])
+  }
+
+  publishToGit() {
+    this.savePackageJson(this.packageJsonGit)
+    this.installAndPublish()
+  }
+
+  publishToNpm() {
+    this.savePackageJson(this.packageJsonNpm)
+    this.installAndPublish()
+  }
+
+  /**
+   * Saves json file locally
+   * @param {string} packageJson package.json content
+   */
+  savePackageJson(packageJson) {
+    const content = JSON.stringify(packageJson, null, '  ')
+    const file = resolve('package.json')
+
+    writeFileSync(file, content)
+  }
+
+  start() {
+    const options = {
+      input: process.stdin,
+      output: process.stdout
+    }
+    const questionMessage = `Which version to set?\nPrevious version: ${this.package.version}\n> `
+
+    this.readline = createInterface(options)
+    this.readline.question(questionMessage, this.handleQuestion)
+    this.readline.on('close', this.handleClose)
+  }
+
+  updatePackageOnGit() {
+    const scripts = [
+      'npm install',
+      'git add package.json',
+      'git add package-lock.json',
+      `git commit -m "chore: Bump application version to ${this.version}"`,
+      `git tag v${this.version}"`,
+      'git push --tags'
+    ]
+
+    this.exec(scripts)
+  }
+
+  updatePackageVersion() {
+    this.package.version = this.version
+
+    this.savePackageJson(this.package)
+    this.updatePackageOnGit()
+  }
 }
 
-updateVersion(package)
+const publishScript = new PublishScript(package)
 
-// cleanup
-exec('git checkout .')
+publishScript.start()
